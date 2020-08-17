9e1dfdd99fefecc6a938a79c03a54caff797c2bb
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-08-17 13:17:18 +0200

    implementing select in register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,18 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="roles" class="form-control" name="roles" required >
+                                    <option value="mentor">Mentor</option>
+                                    <option value="student">Student</option>
+                                </select>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
