41e17f5e7f1276d219ba583863a948c90eade7b6
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-08-19 12:00:50 +0200

    deleting an extra route in web.php

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,3 @@ Route::get('/users', 'UserController@show')->name('users.show');
 Route::get('/profile/{user}','ProfileController@show')->name('profile.show');
 Route::get('/profile/{user}/edit','ProfileController@edit')->name('profile.edit');
 Route::post('/profile/{user}/update','ProfileController@update');
-Route::patch('/profile/{user}/edit','ProfileController@update');
